b5f99635a43845a2882c0159c703a07fef383915
obarra <barra_omar@hotmail.com>
2020-06-28 00:10:54 -0300

    clean

--- src/main/java/com/obarra/accurate/algorithm/Library.java
+++ src/main/java/com/obarra/accurate/algorithm/Library.java
@@ -10,24 +10,24 @@ public class Library {
         long [] minorPathAndValue = {-1, -1, -1, 0};
         long minorAdded = 0;
 
-        for (int j = 0 ;j < 3; j++){
-            int firstPosition = 0;
+        for (int row = 0 ;row < 3; row++){
+            int initialPosition = 0;
             for (int f = 0; f < 3; f++){
-                int firstMovement = j + directionY[f];
+                int firstMovement = row + directionY[f];
                 if (firstMovement < 0 || firstMovement > 2)
                     continue;
                 for (int s = 0; s < 3; s++){
-                    int secondMovement = j + directionY[f] +directionY[s];
+                    int secondMovement = row + directionY[f] +directionY[s];
                     if (secondMovement < 0 || secondMovement > 2)
                         continue;
-                    long result = matrix[j][firstPosition] +
-                            matrix[firstMovement][firstPosition + 1] +
-                            matrix[secondMovement][firstPosition + 2];
+                    long result = matrix[row][initialPosition] +
+                            matrix[firstMovement][initialPosition + 1] +
+                            matrix[secondMovement][initialPosition + 2];
                     if(minorAdded == 0 || minorAdded > result) {
                         minorPathAndValue[3] = minorAdded = result;
-                        minorPathAndValue[0] = matrix[j][firstPosition];
-                        minorPathAndValue[1] = matrix[firstMovement][firstPosition + 1];
-                        minorPathAndValue[2] = matrix[secondMovement][firstPosition + 2];
+                        minorPathAndValue[0] = matrix[row][initialPosition];
+                        minorPathAndValue[1] = matrix[firstMovement][initialPosition + 1];
+                        minorPathAndValue[2] = matrix[secondMovement][initialPosition + 2];
                     }
                 }
             }
