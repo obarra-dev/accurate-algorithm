f3f2edae1743c5186ab87028381fd569561bd9cb
obarra <barra_omar@hotmail.com>
2020-08-14 10:13:51 -0300

    count words

---
+++ src/main/java/com/obarra/accurate/algorithm/StringCounter.java
@@ -0,0 +1,17 @@
+package com.obarra.accurate.algorithm;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class StringCounter {
+
+    public static Map<String, Long> collectWordAmounts(final String phrase) {
+        var wordCollector = new TreeMap<String, Long>();
+        var words = phrase.split(" ");
+        for (String word: words) {
+            wordCollector.compute(word, (key, value)
+                    -> (value == null) ? 1 : value + 1);
+        }
+        return wordCollector;
+    }
+}
---
+++ src/test/java/com/obarra/accurate/algorithm/StringCounterTest.java
@@ -0,0 +1,25 @@
+package com.obarra.accurate.algorithm;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.TreeMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringCounterTest {
+
+    @Test
+    void collectWordAmounts() {
+        var result = StringCounter.collectWordAmounts("java11 java11 omar barra programa en java11 barra");
+
+        var expected = new TreeMap<String, Long>();
+        expected.put("omar", 1L);
+        expected.put("barra", 2L);
+        expected.put("java11", 3L);
+        expected.put("en", 1L);
+        expected.put("programa", 1L);
+        Assertions.assertIterableEquals(expected.entrySet(), result.entrySet());
+    }
+
+}
