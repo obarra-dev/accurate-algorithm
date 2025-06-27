f68a3b45f204208e910e8471ad29801ee4db944d
obarra-dev <obarra@sonatype.com>
2025-06-27 01:06:11 -0300

    pattern strategy

---
+++ algkt/src/main/kotlin/alg/Strategy.kt
@@ -0,0 +1,24 @@
+package alg
+
+typealias Validator = (String) -> Boolean
+
+val emailValidator : Validator = { it.contains('@') }
+val usernameValidator : Validator = { it.isNotEmpty() }
+val passwordValidator : Validator = { it.length >= 4 }
+
+class FormValidator(val name: String, val value: String, private val validator: Validator) {
+    fun isValid() = validator(value)
+}
+
+fun Validator.optional() : Validator = { it.isEmpty() || this(it) }
+
+fun main() {
+    val emailField = FormValidator("email", "omar@test.com", emailValidator.optional())
+    println("email is valid: ${emailField.isValid()}")
+
+    val usernameField = FormValidator("username", "obarra", usernameValidator)
+    println("username is valid: ${usernameField.isValid()}")
+
+    val passwordField = FormValidator("password", "admin123", passwordValidator)
+    println("password is valid: ${emailField.isValid()}")
+}
